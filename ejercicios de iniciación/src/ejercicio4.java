2b5a0e026b47720162e258941f1f97dda45e9072
Jose Luis Rebollo <jlrrebollo176@gmail.com>
2018-10-10 12:17:42 +0200

    ejercicios

--- ejercicios de iniciación/src/ejercicio4.java
+++ ejercicios de iniciación/src/ejercicio4.java
@@ -4,6 +4,7 @@ public class ejercicio4 {
 public static void main(String[] args) {
 	int opcion;
 	
-	System.out.println(arg0);;
+	System.out.println("1.Ginicologia");
+	System.out.println("1.Ginicologia");
 }
 }
